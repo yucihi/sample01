84eef0470ac450e88daf21891e5cc19c8b3f7703
Yuta ISOGAI <isogai@en-corporation.biz>
2015-01-08 21:27:26 +0900

    Book と Review のバリデーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
   has_many :reviews
+
+  validates :title, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :book
+
+  validates :book, presence: true
+  validates :body, presence: true
 end
