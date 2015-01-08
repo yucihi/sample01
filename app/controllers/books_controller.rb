d0b24def517485e4c5e4e112ff1517ec39549f5a
Yuta ISOGAI <isogai@en-corporation.biz>
2015-01-08 21:38:48 +0900

    Books コントローラを実装。Books#index, Books#new ビューを実装。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,45 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new book_params
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
   end
 
   def show
+    @book = Book.find params[:id]
   end
 
   def edit
+    @book = Book.find params[:id]
   end
 
   def update
+    @book = Book.find params[:id]
+    if @book.update_attributes book_params
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find params[:id]
+    @book.destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :isbn, :price)
   end
 end
